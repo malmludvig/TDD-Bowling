7cad819e315b455527e41d6a86386815b5228f52
Ludvig <malmludvig@gmail.com>
2020-12-02 20:54:23 +0100

    The first commit. This is the empty project.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+        public class Person
+        
+        {
+        public string Name;
+        public int Score;
+        }
+
+        public class Round
+
+        {
+
+        private int _personScore;
+
+        private int _pins;
+
+            public void Roll(int pins)
+            {
+                _pins += pins;
+                _personScore += pins;
+            }
+
+            public int Score()
+            {
+                return _pins;
+            }
+            public int PersonScoreMethod()
+            {
+                return _personScore;
+            }
+
+        public bool ScoreZero(int pinsKnockedDown)
+            {
+                if (pinsKnockedDown < 1)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+}
---
+++ UnitTestProject1/BowlingTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+using System.ComponentModel;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class BowlingTests
+    {
+
+
+        
+    }
+}
