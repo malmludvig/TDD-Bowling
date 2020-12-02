93ad46e7021999aec32c6c0489638e7cd78b7e43
Ludvig <malmludvig@gmail.com>
2020-12-02 20:56:29 +0100

    Added classLibrary to make the tests work.

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -2,46 +2,46 @@
 
 namespace ClassLibrary1
 {
-        public class Person
-        
-        {
+    public class Person
+
+    {
         public string Name;
         public int Score;
-        }
+    }
 
-        public class Round
+    public class Round
 
-        {
+    {
 
         private int _personScore;
 
         private int _pins;
 
-            public void Roll(int pins)
-            {
-                _pins += pins;
-                _personScore += pins;
-            }
+        public void Roll(int pins)
+        {
+            _pins += pins;
+            _personScore += pins;
+        }
 
-            public int Score()
-            {
-                return _pins;
-            }
-            public int PersonScoreMethod()
-            {
-                return _personScore;
-            }
+        public int Score()
+        {
+            return _pins;
+        }
+        public int PersonScoreMethod()
+        {
+            return _personScore;
+        }
 
         public bool ScoreZero(int pinsKnockedDown)
+        {
+            if (pinsKnockedDown < 1)
             {
-                if (pinsKnockedDown < 1)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
+                return true;
+            }
+            else
+            {
+                return false;
             }
         }
+    }
 }
